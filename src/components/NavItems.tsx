1376ec7229e85f88df885eab2a782b0d27d85f6a
aniketjha9179 <aniketjha917928@gmail.com>
2023-12-16 21:13:06 +0530

    added escape button with navRef cusom hook

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,15 +1,34 @@
 "use client";
 import { PRODUCT_CATEGORIES } from "@/config";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { NavItem } from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
+  const isAnyOpen = activeIndex !== null;
+  // making escape button work in useE
 
-  const isAnyOpen=activeIndex!==null
+  useEffect(()=>{
+    const handler = (e:KeyboardEvent)=>{
+      if(e.key==='Escape'){
+        setActiveIndex(null);
+      }
+    }
+    // using shorthand of (e)=>hanlder(e)
+    document.addEventListener('keydown', handler);
+    // cleanup function also preventing memeory leak
+    return ()=>{
+      document.removeEventListener('keydown', handler)
+    }
+  },[])
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
 
   return (
-    <div className=" flex  gap-4  h-full">
+    <div className=" flex  gap-4  h-full" ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, i) => {
         const handleOpen = () => {
           if (activeIndex === i) {
